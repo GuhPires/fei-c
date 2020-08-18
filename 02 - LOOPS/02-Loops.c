e37f6c1c034658df9de5554b4173ac0e1dedf3d6
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-18 11:35:49 -0300

    2nd exercise completed

---
+++ 02 - LOOPS/02-Loops.c
@@ -0,0 +1,28 @@
+/************************************** OBJETIVO **************************************************
+ * Escreva um programa que leia números digitados pelo usuário. O programa deve ler os números até
+ * que 0 (zero) seja digitado. Quando 0 for digitado, o programa deve exibir a quantidade de dígitos
+ * que foram digitados, a somatória destes dígitos e a média aritmética.
+ * 
+ * Author: GuhPires
+ * Visit: https://github.com/GuhPires/fei-c
+ *************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  int counter = 0;
+  float digit;
+  float digits_sum = 0;
+  while(digit){
+    printf("Digite um número: ");
+    scanf("%f", &digit);
+    digits_sum += digit;
+    if(digit) counter++;
+  }
+
+  printf("Foram digitados %d números\n", counter);
+  printf("A soma desses números é %.2f\n", digits_sum);
+  printf("A média aritimética é %.2f\n", digits_sum / counter);
+
+  return 0;
+}
