e928273390907c457d8d1858610317d678a8ef18
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-18 11:21:04 -0300

    New set of exercises - LOOPS + Exercise 1 complete

---
+++ 02 - LOOPS/01-Loops.c
@@ -0,0 +1,28 @@
+/************************************** OBJETIVO **************************************************
+ * Escreva um programa que calcula a média aritmética de 5 números digitados pelo usuário. Utilize
+ * contadores e acumuladores.
+ * 
+ * considerando que o primeiro número digitado é n1, segundo n2, ...
+ * media = (n1+n2+n3+n4+n5) / 5
+ * 
+ * Author: GuhPires
+ * Visit: https://github.com/GuhPires/fei-c
+ *************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  int counter = 1;
+  float grades;
+  do {
+    float grade;
+    printf("Digite uma nota: ");
+    scanf("%f", &grade);
+    grades += grade;
+    counter++;
+  } while(counter <= 5);
+
+  printf("A média final é: %.2f", grades / 5);
+
+  return 0;
+}
