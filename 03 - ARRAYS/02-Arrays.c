792a16e205fa9ed0cbcb53addd90f9c325d585e4
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-25 10:23:46 -0300

    Exercise 2 complete

--- 03 - ARRAYS/02-Arrays.c
+++ 03 - ARRAYS/02-Arrays.c
@@ -8,7 +8,22 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int n;
+
+  printf("Digite o número de elementos que deseja digitar: ");
+  scanf("%d", &n);
+
+  int nums[n];
+
+  printf("Agora digite %d números inteiros...\n", n);
+  for(int i = 0; i < n; i++) {
+    printf("Digite o %dº número: ", i + 1);
+    scanf("%d", &nums[i]);
+  }
+
+  for(int i = n - 1; i >= 0; i--){
+    printf("%dº número é %d.\n", i + 1, nums[i]);
+  }
   
   return 0;
 }
