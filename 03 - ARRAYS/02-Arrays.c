e86bd0dd65767335ceced69fa57e759cdc656cac
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-25 10:00:12 -0300

    Exercise 3 complete (skipped 2)

---
+++ 03 - ARRAYS/02-Arrays.c
@@ -0,0 +1,14 @@
+/******************************************* OBJETIVO ******************************************************
+ * Faça um programa que imprime uma sequência de n números em ordem inversa à da leitura.
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  
+  
+  return 0;
+}
--- 03 - ARRAYS/03-Arrays.c
+++ 03 - ARRAYS/03-Arrays.c
@@ -1,6 +1,6 @@
 /******************************************* OBJETIVO ******************************************************
- * Crie um programa em C que peça 10 números reais, armazene-os em um vetor e diga qual é o índice do
- * maior, e seu valor.
+ * Faça um programa para receber um vetor de 10 elementos (pedir para o usuário) e apresentar: a soma dos
+ * ELEMENTOS pares e a soma dos elementos de ÍNDICE par
  * 
  * AUTHOR: GuhPires
  * VISIT: https://github.com/GuhPires/fei-c
@@ -8,21 +8,23 @@
 
 #include <stdio.h>
 
-int main (void) {
-  int higher[2] = {0, 0}, nums[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int main(void) {
+  int even_sum = 0, even_idx_sum = 0, nums[10];
 
   printf("Por favor digite 10 números inteiros...\n");
 
   for(int i = 0; i < 10; i++) {
     printf("Digite o %dº número: ", i + 1);
     scanf("%d", &nums[i]);
-    if(nums[i] > higher[1]) {
-      higher[0] = i;
-      higher[1] = nums[i];
+    if(nums[i] % 2 == 0) {
+      even_sum += nums[i];
+    }
+    if(i % 2 == 0) {
+      even_idx_sum += nums[i];
     }
   }
 
-  printf("O maior valor é %d e está no índice %d.\n", higher[1], higher[0]);
+  printf("Soma dos elementos pares é %d e a soma dos elementos que estão em índice par é %d.\n", even_sum, even_idx_sum);
 
   return 0;
 }
