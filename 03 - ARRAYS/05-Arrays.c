95042df06b3cff0f93a701965e793e4a83bdfe16
Gustavo <gustavo.goncalvespires@gmail.com>
2020-08-25 10:27:09 -0300

    Exercise 5 completed

--- 03 - ARRAYS/05-Arrays.c
+++ 03 - ARRAYS/05-Arrays.c
@@ -8,6 +8,15 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int t[] = {1, 7, 2, 4}, smallest = 0;
+
+  for(int i = 0; i < 4; i++){
+    if(t[i] < smallest || i == 0){
+      smallest = t[i];
+    }
+  }
+
+  printf("O menor número entre [1, 7, 2, 4] é %d.\n", smallest);
+
   return 0;
 }
