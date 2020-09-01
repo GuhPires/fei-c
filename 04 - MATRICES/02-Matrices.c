2e2b99c076b718522f8e645d1b6326e21a431c9b
Gustavo <gustavo.goncalvespires@gmail.com>
2020-09-01 10:00:37 -0300

    2nd exercise completed

---
+++ 04 - MATRICES/02-Matrices.c
@@ -0,0 +1,31 @@
+/******************************************* OBJETIVO ******************************************************
+ * Solicitar dados de uma matriz 4x4 e montar um vetor de 4 elementos com a soma dos elementos ímpares
+ * de cada linha
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  int m[4][4], a[4];
+
+  printf("Popule a matriz...\n");
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      printf("Valor: ");
+      scanf("%d", &m[i][j]);
+      if(m[i][j] % 2 != 0) {
+        a[i] += m[i][j];
+      }
+    }
+  }
+
+  printf("Soma dos elementos ímpares de cada linha:\n");
+  for(int i = 0; i < 4; i++) {
+    printf("Elemento %d: %d\n", i + 1, a[i]);
+  }
+
+  return 0;
+}
