c3bb782e40242cbbccd1365e5ffcb655f08cea3e
Gustavo <gustavo.goncalvespires@gmail.com>
2020-09-01 10:09:23 -0300

    3rd exercise completed

---
+++ 04 - MATRICES/03-Matrices.c
@@ -0,0 +1,37 @@
+/******************************************* OBJETIVO ******************************************************
+ * Faça um programa para receber uma matriz 3×3 (solicitar ao usuário) e apresentar a soma dos elementos da
+ * diagonal principal e a matriz na forma como deve ser vista: com linhas e colunas
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  int m[3][3], main_diagonal = 0; 
+
+  printf("Popule a matriz...\n");
+  for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+      printf("Valor: ");
+      scanf("%d", &m[i][j]);
+    }
+  }
+
+  printf("A matriz digitada: \n");
+  for(int i = 0; i < 3; i++) {
+    printf(" | ");
+    for(int j = 0; j < 3; j++) {
+      printf(" %d ", m[i][j]);
+      if(i == j) {
+        main_diagonal += m[i][j];
+      }
+    }
+    printf("\n");
+  }
+
+  printf("A soma da Diagonal Principal é: %d\n", main_diagonal);
+
+  return 0;
+}
