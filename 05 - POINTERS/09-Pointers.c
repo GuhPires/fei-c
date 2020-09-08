c26045f13f1788db94ce036731ab2bfd27082ec3
Gustavo <gustavo.goncalvespires@gmail.com>
2020-09-08 14:33:46 -0300

    9th exercise

---
+++ 05 - POINTERS/09-Pointers.c
@@ -0,0 +1,20 @@
+/******************************************* OBJETIVO ******************************************************
+ * Crie um programa que contenha uma matriz de float contendo 3 linhas e 3 colunas. Imprima o endereço de
+ * cada posição dessa matriz
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  float m[3][3];
+
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      printf("Endereço da posição %d %d: %p\n", i + 1, j + 1, &m[i][j]);
+    }
+  }
+  return 0;
+}
