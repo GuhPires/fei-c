d356e1336daed5b3ef22fff7dc98b7b763dfd68b
Gustavo <gustavo.goncalvespires@gmail.com>
2020-09-15 10:46:27 -0300

    Exercise 3 completed

---
+++ 06 - MEMORY MANAGEMENT/03-Memory_Management.c
@@ -0,0 +1,30 @@
+/******************************************* OBJETIVO ******************************************************
+ * Escreva um programa em C para alocar memória dinamicamente para armazenar N números inteiros inseridos
+ * pelo usuário e, em seguida, exiba a soma de todos os N números.
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int n = 0, *nums, sum = 0;
+  
+  printf("Digite quantos números deseja inserir: ");
+  scanf("%d", &n);
+  nums = (int*)calloc(n, sizeof(int));
+
+  for(int i = 0; i < n; i++){
+    printf("Digite o %dº número: ", i+1);
+    scanf("%d", &nums[i]);
+    sum += nums[i];
+  }
+
+  printf("A soma dos números digitados é: %d\n", sum);
+
+  free(nums);
+
+  return 0;
+}
