290dcfa9b7347311a3c5eeb6fe288fdbb40613ae
Gustavo <gustavo.goncalvespires@gmail.com>
2020-09-22 10:45:04 -0300

    FUNCTIONS - Exercise 4

--- 07 - FUNCTIONS/04-Functions.c
+++ 07 - FUNCTIONS/04-Functions.c
@@ -13,8 +13,19 @@
 void divide(int, int);
 
 int main(void) {
+  int divider, dividend;
+  printf("Digite o valor que deseja dividir no formato DIVISOR DIVIDENDO: ");
+  scanf("%d %d", &divider, &dividend);
+  divide(divider, dividend);
 
   return 0;
 }
 
-void divide
+void divide(int a, int b) {
+  int count = 0, remainder = a;
+  while(remainder >= b) {
+    remainder -= b;
+    count++;
+  }
+  printf("A divisão de %d por %d é igual a %d com resto %d\n", a, b, count, remainder);
+}
