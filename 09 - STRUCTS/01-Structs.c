a914da083c8949c4e7baf7f5445db9f3d2681680
Gustavo <gustavo.goncalvespires@gmail.com>
2020-10-16 19:58:22 -0300

    STRUCTS - Added project folder + Exercise 1 completed

---
+++ 09 - STRUCTS/01-Structs.c
@@ -0,0 +1,20 @@
+/******************************************* OBJETIVO ******************************************************
+ * Defina uma struct que irá representar bandas de música. Essa estrutura deve ter o nome da banda, que
+ * tipo de música ela toca e o número de integrantes.
+ * 
+ * AUTHOR: GuhPires
+ * VISIT: https://github.com/GuhPires/fei-c
+ **********************************************************************************************************/
+
+#include <stdio.h>
+
+struct my_band {
+  char name[50];
+  char genre[100];
+  int participants;
+};
+
+int main(void) {
+  struct my_band doidera;
+  return 0;
+}
