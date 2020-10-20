efbda3d6517ba74bb0a9c840ceb4fad694e8ff9e
Gustavo <gustavo.goncalvespires@gmail.com>
2020-10-20 14:38:26 -0300

    STRUCTS - Exercise 6 completed

--- 09 - STRUCTS/06-Structs.c
+++ 09 - STRUCTS/06-Structs.c
@@ -8,6 +8,8 @@
  **********************************************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 struct card {
   // D -> diamonds; H -> hearts; C -> clubs; S -> spades
@@ -15,6 +17,66 @@ struct card {
   char value;
 };
 
+void print_deck(struct card*);
+void distribute_cards(struct card*);
+
 int main(void) {
+  // Usando o tempo como Seed pra função rand() 
+  srand(time(0));
+
+  // 52 cartas em um baralho
+  struct card deck[52];
+  char symbols[4] = {'D', 'H', 'C', 'S'};
+  // Valor 0 = carta de valor 10 (para não ser lido como uma string ao invés de char)
+  char values[13] = {'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'Q', 'J', 'K'};
+  int iter = 0;
+  for(int i = 0; i < 4; i++){
+    for(int j = 0; j < 13; j++) {
+      deck[iter].symbol = symbols[i];
+      deck[iter].value = values[j];
+      iter++;
+    }
+  }
+
+  print_deck(deck);
+
+  distribute_cards(deck);
   return 0;
+}
+
+void print_deck(struct card deck[]) {
+  printf("\nTodas as cartas do baralho...\n");
+  for(int i = 0; i < 52; i++){
+    printf("%c de %c\n", deck[i].value, deck[i].symbol);
+  }
+  printf("-------------------------------\n");
+}
+
+void distribute_cards(struct card deck[]) {
+  struct card p_one[5];
+  struct card p_two[5];
+
+  for(int i = 0; i < 10; i++) {
+    // Lógica geral da função rand(): (rand() % (upper - lower + 1)) + lower
+    int num = rand() % 53;
+    if(i < 5) {
+      p_one[i].symbol = deck[num].symbol;
+      p_one[i].value = deck[num].value;
+    } else {
+      p_two[i - 5].symbol = deck[num].symbol;
+      p_two[i - 5].value = deck[num].value;
+    }
+  }
+
+  printf("==== Cartas do Jogador 1 ====\n");
+  for(int i = 0; i < 5; i++) {
+    printf("%c de %c\n", p_one[i].value, p_one[i].symbol);
+  }
+  printf("=============================\n");
+
+  printf("==== Cartas do Jogador 2 ====\n");
+  for(int i = 0; i < 5; i++) {
+    printf("%c de %c\n", p_two[i].value, p_two[i].symbol);
+  }
+  printf("=============================\n");
 }
