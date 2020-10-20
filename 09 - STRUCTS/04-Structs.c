3cad382d0e3886af97f033e7aa43582c7cfa94ff
Gustavo <gustavo.goncalvespires@gmail.com>
2020-10-20 11:42:33 -0300

    STRUCTS - Exercise 4 completed

--- 09 - STRUCTS/04-Structs.c
+++ 09 - STRUCTS/04-Structs.c
@@ -7,17 +7,15 @@
  **********************************************************************************************************/
 
 #include <stdio.h>
+#include <math.h>
 
-struct ponto {
+struct point {
   int x;
   int y;
 };
 
 struct rectangle {
-  struct ponto p1;
-  struct ponto p2;
-  struct ponto p3;
-  struct ponto p4;
+  struct point points[4];
 };
 
 float calc_perimeter(struct rectangle);
@@ -25,14 +23,24 @@ float calc_area(struct rectangle);
 
 int main(void) {
   struct rectangle rect;
-
+  int coordinates[4][2] = { {0, 0}, {0, 4}, {6, 4}, {6, 0} };
+  for(int i = 0; i < 4; i++) {
+    rect.points[i].x = coordinates[i][0];
+    rect.points[i].y = coordinates[i][1];
+  }
+  printf("O tamanho do perímetro é %f\n", calc_perimeter(rect));
+  printf("O tamanho da área é %f\n", calc_area(rect));
   return 0;
 }
 
 float calc_perimeter(struct rectangle r) {
-  return ;
+  float height = sqrt(pow((r.points[1].x - r.points[0].x), 2) + pow((r.points[1].y - r.points[0].y), 2));
+  float width = sqrt(pow((r.points[3].x - r.points[0].x), 2) + pow((r.points[3].y - r.points[0].y), 2));
+  return (height * 2) + (width * 2);
 }
 
 float calc_area(struct rectangle r) {
-  return ;
+  float height = sqrt(pow((r.points[1].x - r.points[0].x), 2) + pow((r.points[1].y - r.points[0].y), 2));
+  float width = sqrt(pow((r.points[3].x - r.points[0].x), 2) + pow((r.points[3].y - r.points[0].y), 2));
+  return height * width;
 }
